3aeccfbf3c4f38a86f8e751c57db307521bb82e5
Chinwuba Israel <chiizzy5@gmail.com>
2025-02-17 20:36:49 +0100

    update color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer: FC = ({}) => {
     <footer className="">
       <div className="container max-w-[1342px] mx-auto w-full">
         <div className="flex items-center justify-center gap-2 py-[18px]">
-          <LuCopyright size={32} className="text-black" />
-          <span className="text-[14px] leading-[19.2px] -tracking-[3.5%] font-bold font-monumentExtended text-center text-black">
+          <LuCopyright size={32} className="text-tertiary"/>
+          <span className="text-[14px] leading-[19.2px] -tracking-[3.5%] font-bold font-monumentExtended text-center text-tertiary">
             {new Date().getFullYear()} By
           </span>
           <Link href="https://x.com/samurai_10x">samurai</Link>
